d82ab963ee7097d62094e1b9aad25da2b650f89f
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-06-03 08:52:25 +0000

    commit6_03.06.24

--- spezisische_Mineralsuche.py
+++ spezisische_Mineralsuche.py
@@ -23,8 +23,7 @@ MINDAT_API_URL = "https://api.mindat.org"
 # select Information that schould be displayed
 selection = st.multiselect(
     label=label_selectbox,
-    options=['name', 'mindat_formula', 'ima_formula', 'description_short', 'elements', 'z', 'shortcode_ima']
-)
+    options=['name', 'mindat_formula', 'ima_formula', 'description_short', 'elements', 'z', 'shortcode_ima'])
 
 # definition of the important minerals
 important_minerals = [
@@ -41,8 +40,7 @@ important_minerals = [
     "Chromite", "Magnetite", "Tremolite", "Actinolite", "Glaucophane",
     "Riebeckite", "Lizardite", "Augite", "Chrysotile", "Antigorite",
     "Talc", "Chlorite", "Clinochlore", "Chamosite", "Tourmaline",
-    "Lawsonite", "Epidote", "Zoisite", "Olivine", "Zircon", "Titanite", "Staurolite", "Apatite", "Monazite"
-]
+    "Lawsonite", "Epidote", "Zoisite", "Olivine", "Zircon", "Titanite", "Staurolite", "Apatite", "Monazite"]
 
 # Function to check if the response is valid JSON
 def is_valid_json(response):
@@ -86,7 +84,9 @@ if st.button(label=label_button, use_container_width=True):
     if all_results:
 
         # Filter the results to include only the selected fields
+
         filtered_results = []
+        
         for result in all_results:
             filtered_result = {field: result.get(field, None) for field in selection}
             filtered_results.append(filtered_result)
@@ -97,11 +97,9 @@ if st.button(label=label_button, use_container_width=True):
         with open(json_path, 'w') as json_file:
             json_file.write(json_data)
 
-        
-
         # Display results in dropdown format
         for item in filtered_results:
-            name = item.get("name", "Unnamed Mineral")
+            name = item.get("name")
             with st.expander(name):
                 for key, value in item.items():
                     if isinstance(value, list):
@@ -116,4 +114,4 @@ if st.button(label=label_button, use_container_width=True):
             mime='application/json'
         )
     else:
-        st.warning("No data retrieved.")
+        st.warning('Failed to fetch data.')
