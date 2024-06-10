c9c801ea9fa02b301e5d0a3372755afce69dc2fa
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-06-10 13:00:15 +0000

    c1_10.06.24

--- spezisische_Mineralsuche.py
+++ spezisische_Mineralsuche.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import requests
 import json
+import pandas as pd
 
 # text and information 
 header = 'An Overview of the Most Important Minerals'
@@ -20,11 +21,21 @@ st.divider()
 key = st.secrets["api_key"]
 MINDAT_API_URL = "https://api.mindat.org"
 
+# definition of the important minerals
+
+important_minerals = pd.read_csv('/workspaces/Test_Auswahl_Minerale/data/important_minerals.csv')
+
+# select mineral
+
+mineral=st.selectbox('Select Mineral', important_minerals)
+
 # select Information that schould be displayed
+
 selection = st.multiselect(
     label=label_selectbox,
     options=['name', 'mindat_formula', 'ima_formula', 'aboutname', 'elements', 'z', 'shortcode_ima'])
 
+<<<<<<< HEAD
 # definition of the important minerals
 # important_minerals = '/data/mineral_names.csv'
 important_minerals = [
@@ -42,6 +53,8 @@ important_minerals = [
     "Riebeckite", "Lizardite", "Augite", "Chrysotile", "Antigorite",
     "Talc", "Chlorite", "Clinochlore", "Chamosite", "Tourmaline",
     "Lawsonite", "Epidote", "Zoisite", "Olivine", "Zircon", "Titanite", "Staurolite", "Apatite", "Monazite"]
+=======
+>>>>>>> 34a4f77 (c1_10.06.24)
 
 st.selectbox('Select Mineral', important_minerals)
 
@@ -58,63 +71,61 @@ if st.button(label=label_button, use_container_width=True):
     st.write(info)
 
     all_results = []
+    filter_dict = {"name": mineral, 'format': 'json'}
+    headers = {'Authorization': 'Token ' + key}
+    params = filter_dict
 
-    for i, mineral in enumerate(important_minerals):
-        filter_dict = {"name": mineral, 'format': 'json'}
-        headers = {'Authorization': 'Token ' + key}
-        params = filter_dict
+    try:
+        response = requests.get(MINDAT_API_URL + "/geomaterials/", params=params, headers=headers)
+        if response.status_code == 200 and is_valid_json(response):
+            result_data = response.json().get("results", [])
+            all_results.extend(result_data)
 
-        try:
-            response = requests.get(MINDAT_API_URL + "/geomaterials/", params=params, headers=headers)
-            if response.status_code == 200 and is_valid_json(response):
-                result_data = response.json().get("results", [])
-                all_results.extend(result_data)
+            while response.json().get("next"):
+                next_url = response.json()["next"]
+                response = requests.get(next_url, headers=headers)
+                if response.status_code == 200 and is_valid_json(response):
+                    result_data = response.json().get("results", [])
+                    all_results.extend(result_data)
+                else:
+                    break
+        else:
+            st.error(f"Failed to fetch data for {mineral}: {response.status_code}")
+            st.error(f"Response content: {response.text}")
+    except requests.RequestException as e:
+        st.error(f"Request failed for {mineral}: {e}")
 
-                while response.json().get("next"):
-                    next_url = response.json()["next"]
-                    response = requests.get(next_url, headers=headers)
-                    if response.status_code == 200 and is_valid_json(response):
-                        result_data = response.json().get("results", [])
-                        all_results.extend(result_data)
-                    else:
-                        break
-            else:
-                st.error(f"Failed to fetch data for {mineral}: {response.status_code}")
-                st.error(f"Response content: {response.text}")
-        except requests.RequestException as e:
-            st.error(f"Request failed for {mineral}: {e}")
+if all_results:
 
-    if all_results:
+    # Filter the results to include only the selected fields
 
-        # Filter the results to include only the selected fields
+    filtered_results = []
 
-        filtered_results = []
+    for result in all_results:
+        filtered_result = {field: result.get(field, None) for field in selection}
+        filtered_results.append(filtered_result)
 
-        for result in all_results:
-            filtered_result = {field: result.get(field, None) for field in selection}
-            filtered_results.append(filtered_result)
+    # Write results to a JSON file
+    json_data = json.dumps(filtered_results, indent=4)
+    json_path = 'mineral_data.json'
+    with open(json_path, 'w') as json_file:
+        json_file.write(json_data)
 
-        # Write results to a JSON file
-        json_data = json.dumps(filtered_results, indent=4)
-        json_path = 'mineral_data.json'
-        with open(json_path, 'w') as json_file:
-            json_file.write(json_data)
+    # Display results in dropdown format
+    for item in filtered_results:
+        name = item.get("name")
+        with st.expander(name):
+            for key, value in item.items():
+                if isinstance(value, list):
+                    value = ', '.join(value)
+                st.write(f"**{key.capitalize()}:** {value}")
 
-        # Display results in dropdown format
-        for item in filtered_results:
-            name = item.get("name")
-            with st.expander(name):
-                for key, value in item.items():
-                    if isinstance(value, list):
-                        value = ', '.join(value)
-                    st.write(f"**{key.capitalize()}:** {value}")
-
-        # Display download button
-        st.download_button(
-            label=label_button_2,use_container_width=True,
-            data=json_data,
-            file_name='mineral_data.json',
-            mime='application/json'
-        )
-    else:
-        st.warning('Failed to fetch data.')
+    # Display download button
+    st.download_button(
+        label=label_button_2,use_container_width=True,
+        data=json_data,
+        file_name='mineral_data.json',
+        mime='application/json'
+    )
+else:
+    st.warning('Failed to fetch data.')
