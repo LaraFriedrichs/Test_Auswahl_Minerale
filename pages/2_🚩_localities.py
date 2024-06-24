b98107206aabe054f879d85e2da88bfb7b7fbec2
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-06-24 12:31:02 +0000

    24.06.2024c1

--- pages/2_🚩_localities.py
+++ pages/2_🚩_localities.py
@@ -1,2 +1,41 @@
 import streamlit as st
-st.write('TEST')
+import requests
+
+MINDAT_API_URL = "https://api.mindat.org"
+key=st.secrets["api_key"]
+field_str='name,id'
+headers = {'Authorization': 'Token ' + key}
+params = {'format': 'json','fields':'field_str'}
+all_results = []
+
+def is_valid_json(response):
+    try:
+        response.json()
+        return True
+    except ValueError:
+        return False
+
+try:
+    response = requests.get(MINDAT_API_URL + "/minerals_ima/", params=params, headers=headers)
+    if response.status_code == 200 and is_valid_json(response):
+        result_data = response.json().get("results", [])
+        all_results.extend(result_data)
+
+        while response.json().get("next"):
+            next_url = response.json()["next"]
+            response = requests.get(next_url, headers=headers)
+            if response.status_code == 200 and is_valid_json(response):
+                result_data = response.json().get("results", [])
+                all_results.extend(result_data)
+            else:
+                break
+    else:
+        st.error("Failed to fetch data")
+        #st.error(f"Response content: {response.text}")
+except requests.RequestException as e:
+    st.error("Request failed")
+all_results
+
+#"latitude": 0.1,
+#"longitude": 0.1,
+# https://api.mindat.org/localities/{id}/
