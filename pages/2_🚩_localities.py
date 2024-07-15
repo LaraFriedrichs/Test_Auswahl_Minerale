2b0d95f9b445310c29395ccc021760d45f925744
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-07-15 10:21:46 +0000

    c12

--- pages/2_🚩_localities.py
+++ pages/2_🚩_localities.py
@@ -66,7 +66,7 @@ st.markdown(f"Check out the [Mindat.org page](https://www.mindat.org/min-{id}.ht
 all_localities=[]
 params = {"id":id,"format":"json"}
 try:
-    response = requests.get(MINDAT_API_URL + f"/localities/{id}/", params=params, headers=headers)
+    response = requests.get(MINDAT_API_URL + f"/geomaterials/{id}/", params=params, headers=headers)
     if response.status_code == 200 and is_valid_json(response):
         result_data = response.json().get("results", [])
         all_results.extend(result_data)
