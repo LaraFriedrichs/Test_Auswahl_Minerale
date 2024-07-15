91f9c0783d51bb026bfa9e4ee78a7307134ed357
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-07-15 10:02:37 +0000

    c7

--- pages/2_🚩_localities.py
+++ pages/2_🚩_localities.py
@@ -64,6 +64,7 @@ st.markdown(f"Check out the [Mindat.org page](https://www.mindat.org/min-{id}.ht
 
 ############# Get the localities for  the important minerals########
 all_localities=[]
+params = {"name": mineral, "id":id}
 try:
     response = requests.get(MINDAT_API_URL + f"/locobject/{id}/", params=params, headers=headers)
     if response.status_code == 200 and is_valid_json(response):
