6589051acbd1ccf830dcede75b5d3d75e58e4bbd
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-08-26 10:48:30 +0000

    c2

--- pages/2_🔎_shortcodes.py
+++ pages/2_🔎_shortcodes.py
@@ -1,8 +1,3 @@
-
-
-
-############################## Old code ############################
-
 import streamlit as st
 import requests
 
@@ -52,6 +47,35 @@ api_fields=['name','shortcode_ima']
 params = {"ima_status": "APPROVED", "format": "json"}
 all_results = []
 
+#try:
+    #response = requests.get(MINDAT_API_URL + "/geomaterials/", params=params, headers=headers)
+    #while response.status_code == 200 and is_valid_json(response):
+        #response_data = response.json()
+        #result_data = response_data.get("results", [])
+        #all_results.extend(result_data)
+
+        #next_url = response_data.get("next")
+        #if not next_url:
+           # break
+        #response = requests.get(next_url, headers=headers)
+#except requests.RequestException as e:
+    #st.error(f"Request failed! {e}")
+
+#all_results
+
+#results=[]
+
+#for entry in all_results:
+    #name = entry["name"]
+    #short_code = entry["shortcode_ima"]
+    #results.append(short_code)
+
+#results
+
+
+#####################
+
+
 try:
     response = requests.get(MINDAT_API_URL + "/geomaterials/", params=params, headers=headers)
     while response.status_code == 200 and is_valid_json(response):
@@ -64,13 +88,27 @@ try:
             break
         response = requests.get(next_url, headers=headers)
 except requests.RequestException as e:
-    st.error(f"Request failed! {e}")
+    st.error("Request failed")
 
-results=[]
+if all_results:
+    filtered_results = []
+    # Filter the results to include only the selected fields
+    for result in all_results:
+        filtered_result = {all_results[field] : result.get(field) for field in api_fields}
+        filtered_results.append(filtered_result)
 
-for entry in all_results:
-    name = entry["name"]
-    short_code = entry["shortcode_ima"]
-    results.append(short_code)
 
-results
+    
+    df = pd.DataFrame.from_dict(pd.json_normalize(filtered_results), orient='columns')
+        
+    df
+############################################# download results ################################################################
+    st.divider()
+    st.subheader(subheader_4)
+    st.write(info_2)
+    st.download_button(
+        label=label_button, use_container_width=True,
+        data=json.dumps(filtered_results, indent=4),
+        file_name='mineral_data.json',
+        mime='application/json')
+
