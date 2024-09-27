7ca2808f9ce9e42fbf5db7bc91eaf59ac8614d4a
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-09-27 14:14:15 +0200

    yml update

--- request.py
+++ request.py
@@ -3,31 +3,17 @@ import json
 import requests
 import pandas as pd
 
-<<<<<<< HEAD
-
-from utils.func import is_valid_json
-
-=======
 def is_valid_json(response):
     try:
         response.json()
         return True
     except ValueError:
         return False
->>>>>>> 23387ee88348cd4db502782beddbb10aca9ecd6e
 
 # API-Schlüssel aus Umgebungsvariablen laden
 key = os.getenv("MINDAT_API_KEY")
 MINDAT_API_URL = "https://api.mindat.org"
 
-<<<<<<< HEAD
-
-
-url_1 = "https://raw.githubusercontent.com/LaraFriedrichs/Test_Auswahl_Minerale/main/data/important_minerals.csv"
-
-
-important_minerals = pd.read_csv(url_1)
-=======
 if not key:
     raise ValueError("No API-Key found.")
     
@@ -38,7 +24,6 @@ important_minerals = ["Pyrope","Almandine",
 "Rutile", "Hematite", "Ilmenite","Chromite","Magnetite","Tremolite","Actinolite","Glaucophane","Riebeckite","Lizardite","Augite","Chrysotile","Antigorite","Talc",
 "Chlorite","Clinochlore","Chamosite","Tourmaline","Lawsonite","Epidote","Zoisite","Fayalite","Forsterite","Zircon","Titanite","Staurolite","Apatite","Monazite"]
 
->>>>>>> 23387ee88348cd4db502782beddbb10aca9ecd6e
 all_results_stored = []
 
 for mineral in important_minerals:
@@ -66,8 +51,4 @@ if all_results_stored:
     with open(output_file_path, 'w', encoding='utf-8') as json_file:
         json.dump(all_results_stored, json_file, ensure_ascii=False, indent=4)
 else:
-<<<<<<< HEAD
     print("No results found.")
-=======
-    print("No results found.")
->>>>>>> 23387ee88348cd4db502782beddbb10aca9ecd6e
