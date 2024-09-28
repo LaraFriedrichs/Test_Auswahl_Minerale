33b5a12fe078dd878be1c6379fe06db414279155
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-09-28 09:59:01 +0200

    test

--- Home.py
+++ Home.py
@@ -66,6 +66,8 @@ with tab1:
 
     key = st.secrets["api_key"]
     MINDAT_API_URL = "https://api.mindat.org"
+    if not key:
+        raise ValueError("No API-Key found.")
 
 ######################################################### Important minerals and field mapping ###########################################################
 
@@ -280,6 +282,34 @@ with tab2:
     #             st.write(f"No results found for shortcode '{shortcode}'.")
     # else:
     #     st.write('Please select at least one shortcode.')
-    
+    all_results_stored = []
+
+    for mineral in important_minerals:
+
+        params = {"name": mineral, "ima_status": "APPROVED", "format": "json"}
+        headers = {'Authorization': 'Token ' + key}
+
+        try:
+            response = requests.get(MINDAT_API_URL + "/geomaterials/", params=params, headers=headers)
+            while response.status_code == 200 and is_valid_json(response):
+                response_data = response.json()
+                result_data = response_data.get("results", [])
+                all_results_stored.extend(result_data)
+
+                next_url = response_data.get("next")
+                if not next_url:
+                    break
+                response = requests.get(next_url, headers=headers)
+        except requests.RequestException as e:
+            print(f"Request failed for {mineral}: {e}")
+
+    # Speichern als JSON-Datei im 'data'-Ordner
+    if all_results_stored:
+        output_file_path = os.path.join('data', 'mineral_results.json')
+        with open(output_file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(all_results_stored, json_file, ensure_ascii=False, indent=4)
+    else:
+        print("No results found.")
+
     
         
